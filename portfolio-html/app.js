648015fe1932ad755dff010f1d21351ca1b2cf5e
jmackert <jonahbmackert@gmail.com>
2019-10-08 18:25:40 -0400

    added carousel

---
+++ portfolio-html/app.js
@@ -0,0 +1,51 @@
+(function() {
+
+
+
+    addImageCarousel();
+
+
+
+    // Image Carousel
+    function addImageCarousel(){
+
+        const carouselFrame = document.getElementById("carouselFrame");
+        const carouselCaption = document.getElementById("carouselCaption");
+
+        const carouselSlides =
+            Array.from(carouselFrame.querySelectorAll("div[data-src]"))
+                .map(div => {
+                    return {
+                        imageUrl: div.getAttribute("data-src"),
+                        caption: div.textContent.trim(),
+                    };
+                });
+        
+        let carouselIndex = 0;
+        displayCarouselImage();
+
+        function displayCarouselImage(){
+            let {imageUrl,caption} = carouselSlides[carouselIndex];
+            carouselFrame.style.backgroundImage = `url('${imageUrl}')`;
+            let count = `(${carouselIndex+1}/${carouselSlides.length})`;
+            carouselCaption.innerHTML = count + caption;
+            carouselCaption.classList.add("flash");
+            setTimeout(() => {
+                carouselCaption.classList.remove("flash");
+            }, 0);
+        }
+
+        function displayNextCarouselImage(){
+            carouselIndex++;
+            if(carouselIndex === carouselSlides.length){
+                carouselIndex = 0;
+            }
+            displayCarouselImage();
+        }
+
+        setInterval(function() {
+            displayNextCarouselImage();
+        }, 4000);
+    }
+
+})();
