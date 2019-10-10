cab00d69271c88d0a92d34fa71d7936a06219040
jmackert <jonahbmackert@gmail.com>
2019-10-10 14:26:39 -0400

    added dropdown for projects info

--- portfolio-html/app.js
+++ portfolio-html/app.js
@@ -1,10 +1,7 @@
 (function() {
 
-
-
     addImageCarousel();
-
-
+    addCollapsible();
 
     // Image Carousel
     function addImageCarousel(){
@@ -44,6 +41,26 @@
         setInterval(function() {
             displayNextCarouselImage();
         }, 3000);
+    }
+
+ 
+    //Collapsible
+    function addCollapsible(){
+        var collapsible = document.getElementsByClassName("collapsible");
+        var i;
+
+        for(i=0; i < collapsible.length; i++){
+            collapsible[i].addEventListener("click", function(){
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if(content.style.maxHeight){
+                    content.style.maxHeight = null;
+                }
+                else{
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
+        }
     }
 
 })();
